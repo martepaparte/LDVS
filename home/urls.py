cd012ac61907060a37e8f9a2c5969f459ddf6ac4
martepaparte <martyna.dadurkeviciute@gmail.com>
2021-10-24 23:06:50 +0300

    added user login

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.LoginInterfaceView.as_view()),
-    path('welcome', views.WelcomeView.as_view()),
+    path('login', views.LoginInterfaceView.as_view(), name='login'),
+    path('welcome', views.WelcomeView.as_view(), name='welcome'),
+    path('userlogout', views.LogoutInterfaceView.as_view(), name='userlogout'),
+    # url(r'^logout/$', LogoutView.as_view(), name='logout')
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,18 @@
+from django.contrib.auth import logout
 from django.shortcuts import render
 from django.contrib.auth.views import TemplateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 class LoginInterfaceView(LoginView):
     template_name = 'home/login.html'
 
+
 class WelcomeView(TemplateView):
     template_name = 'home/welcome.html'
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+
+
